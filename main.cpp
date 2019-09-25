f239e9e668368563c859a8106bae33c0821f8a04
NathanLovell <nslovell@mail.csuchico.edu>
2019-09-25 11:25:21 -0700

    Fixes issue #4. Players can play again at end of game

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@ int main()
   char decision1 = ' ';
   char decision2 = ' ';
 
+  //if the people want to play playagain
+  int play_again = 2;
+
 
   cout<<"Player 1, please enter your name: ";
   cin>>player1name;
@@ -20,41 +23,52 @@ int main()
   cin>>player2name;
 
   cout<<"WELCOME TO SPLIT OR STEAL!\n";
-
-  cout<<player1name<<", please enter the character of your choice...\n";
-  cout<<"Please enter (s)plit or s(t)eal: ";
-  cin>>decision1;
-
-  cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-      <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-      <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-      <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-
-  cout<<player2name<<", please enter the character of your choice...\n";
-  cin>>decision2;
-
-  if(decision1 == 't' && decision2 == 's')
-  {
-    cout<<player1name<<" stole while "<<player2name<<" split. "
-        <<player1name<<" WINS ALL OF THE PRIZE!\n";
-  }
-  else if(decision2 == 't' && decision1 == 's')
-  {
-    cout<<player2name<<" stole while "<<player1name<<" split. "
-        <<player2name<<" WINS ALL OF THE PRIZE!\n";
-  }
-  else if(decision1 == 's' && decision2 == 's')
-  {
-    cout<<"Both players cooperated and chose to split. Congrats, "
-        <<player1name<<" and "<<player2name<<" you each get half the prize!\n";
-  }
-  else
-  {
-    cout<<"Sorry, you were both greedy and chose to steal. Neither of you "
-        <<"win a prize and go home with NOTHING!\n";
-  }
+  do{
 
 
+    cout<<player1name<<", please enter the character of your choice...\n";
+    cout<<"Please enter (s)plit or s(t)eal: ";
+    cin>>decision1;
+
+    cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+        <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+        <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+        <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+
+    cout<<player2name<<", please enter the character of your choice...\n";
+    cin>>decision2;
+
+    if(decision1 == 't' && decision2 == 's')
+    {
+      cout<<player1name<<" stole while "<<player2name<<" split. "
+          <<player1name<<" WINS ALL OF THE PRIZE!\n";
+    }
+    else if(decision2 == 't' && decision1 == 's')
+    {
+      cout<<player2name<<" stole while "<<player1name<<" split. "
+          <<player2name<<" WINS ALL OF THE PRIZE!\n";
+    }
+    else if(decision1 == 's' && decision2 == 's')
+    {
+      cout<<"Both players cooperated and chose to split. Congrats, "
+          <<player1name<<" and "<<player2name<<" you each get half the prize!\n";
+    }
+    else
+    {
+      cout<<"Sorry, you were both greedy and chose to steal. Neither of you "
+          <<"win a prize and go home with NOTHING!\n";
+    }
+
+    cout << "Would you like to play again? 1: Yes  2: No"
+    do{
+      cin >> play_again;
+      if(play_again != 1 || play_again != 2){
+        cout << "Please enter either 1 or 2" << endl;
+      }
+    }while(play_again != 1 || play_again != 2);
+}while(playagain == 1);
+
+cout << "Have a great day!" << endl;
 
 
 
